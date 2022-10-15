86b3b510ccc139d3baf362e87e90a59f119fdcdf
oliver <minakawa.daiki.as@gmail.com>
2022-10-16 03:18:15 +0900

    implement contract and add test

--- packages/solidity-ts/contracts/Amidakuji.sol
+++ packages/solidity-ts/contracts/Amidakuji.sol
@@ -2,50 +2,150 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Amidakuji {
-  // event SetPurpose(address sender, string purpose);
-  //string public purpose = "Building Unstoppable Apps!";
+contract Amidakuji is Ownable {
+  uint256 public currentGameId;
+  uint256 public entryTimeDuration = 1 days;
+  uint256 public drawTimeDuration = 10 minutes;
 
-  constructor() {}
-
-  /*
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      // emit SetPurpose(msg.sender, purpose);
-  }
-  */
-
-  function setup() public returns (uint256) {
-    // ownable
-    // setup a new game
-    return 1; // gameId
+  // TODO uint256 を最適化
+  struct Game {
+    uint256 id;
+    uint256 startTime;
+    uint256 maxPlayerCount;
+    uint256 atariPos;
+    address[] players;
+    uint256[] playersPos;
+    uint256[] line;
+    bool revealed;
   }
 
-  function game(uint256 gameId) public returns (string memory) {
-    return "game data";
+  struct BeforeRevealGame {
+    uint256 id;
+    uint256 startTime;
+    uint256 drawingStartTime;
+    uint256 endTime;
+    address[] players;
   }
 
-  function entry() public returns (bool) {
+  mapping(uint256 => Game) private _games;
+
+  constructor() {
+    currentGameId = 0;
+  }
+
+  function _indexOf(uint256[] memory arr, uint256 searchFor) private pure returns (uint256) {
+    for (uint256 i = 0; i < arr.length; i++) {
+      if (arr[i] == searchFor) {
+        return i;
+      }
+    }
+    revert("Not found");
+  }
+
+  // TODO oracleを使った方が安全
+  function _randMod(uint256 _nonce, uint256 _modulus) private view returns (uint256) {
+    return uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, _nonce))) % _modulus;
+  }
+
+  // TODO 最新のチェックができるなら、誰でも叩けていい気がしている
+  // setup (by owner): ゲームの初期設定。あたりの場所をprivate変数に格納
+  function setup(uint256 _nonce) public onlyOwner returns (uint256) {
+    // TODO 最新がrevealedになってるかどうかのチェック
+
+    currentGameId++;
+    uint256 startTime = block.timestamp;
+    address[] memory players;
+    uint256[] memory playersPos;
+    uint256[] memory line;
+
+    Game memory newGame = Game({
+      id: currentGameId,
+      startTime: startTime,
+      maxPlayerCount: 6, // TODO 一旦6人まで
+      atariPos: _randMod(_nonce, 6), // TODO 一旦AからFまで。 A:0 ~ F:6
+      players: players,
+      playersPos: playersPos,
+      line: line,
+      revealed: false
+    });
+
+    _games[currentGameId] = newGame;
+
+    return newGame.id;
+  }
+
+  // entry: 自分の初期位置の設定（＆ゲームへのエントリー）
+  function entry(uint256 _id, uint32 _pos) public returns (bool) {
+    if (_games[_id].id == 0) revert("Not exists");
+    if (_games[_id].startTime + entryTimeDuration < block.timestamp) revert("Ended entry time");
+
+    // 重複参加のチェック
+    for (uint256 i = 0; i < _games[_id].players.length; i++) {
+      if (_games[_id].players[i] == msg.sender) {
+        revert("Already exists");
+      }
+    }
+
+    // ポジションの重複チェック
+    for (uint256 i = 0; i < _games[_id].playersPos.length; i++) {
+      if (_games[_id].playersPos[i] == _pos) {
+        revert("Already exists");
+      }
+    }
+
+    _games[_id].players.push(msg.sender);
+    _games[_id].playersPos.push(_pos);
     return true; // succeed
-    // store entry data to storage (public?)
   }
 
-  function draw(uint256 kuji, uint256 pos) public returns (bool) {
+  // draw: 線を引く。private領域へ情報を格納。参加人数と同じ回数線を引ける。
+  // TODO 誰がどこに弾いたかを見れるようにする？
+  function draw(uint256 _id, uint256 startPos) public returns (bool) {
+    if (_games[_id].startTime + entryTimeDuration > block.timestamp) revert("Not Started drawing time");
+    if (_games[_id].startTime + entryTimeDuration + drawTimeDuration < block.timestamp) revert("Ended drawing time");
+
+    // 参加者チェック
+    bool canDraw = false;
+    for (uint256 i = 0; i < _games[_id].players.length; i++) {
+      if (_games[_id].players[i] == msg.sender) {
+        canDraw = true;
+      }
+    }
+    if (!canDraw) {
+      revert("Does not entry");
+    }
+
+    if (startPos < 0 && startPos > 5) revert("Invalid Position"); // TODO 一旦 A:0 ~ E:5 をマッピング
+    _games[_id].line.push(startPos);
     return true;
-    // store drawing info to private storage
   }
 
-  function reveal() public {
-    // ownable
-    // move private storage data to public one
+  // TODO 誰でも叩けていい気がしている
+  // reveal (by owner): revealedを変えるだけ。
+  function reveal(uint256 _id) public onlyOwner {
+    if (_games[_id].startTime + entryTimeDuration + drawTimeDuration > block.timestamp) revert("Not Ended drawing time");
+    _games[_id].revealed = true;
+    // TODO 報酬の分配
   }
 
-  function result() public {
-    // return winner id? address?
+  // result: ゲーム結果を得る（実態はリビールされたGame Structを返している）
+  function result(uint256 _id) public view returns (Game memory) {
+    if (_games[_id].revealed == false) revert("Not revealed");
+    return _games[_id];
+  }
+
+  // reveal前のGameを返す
+  function game(uint256 _id) public view returns (BeforeRevealGame memory) {
+    BeforeRevealGame memory _game = BeforeRevealGame({
+      id: _games[_id].id,
+      startTime: _games[_id].startTime,
+      drawingStartTime: _games[_id].startTime + entryTimeDuration,
+      endTime: _games[_id].startTime + entryTimeDuration + drawTimeDuration,
+      players: _games[_id].players
+    });
+    return _game;
   }
 }
--- packages/solidity-ts/tests/hardhat-tests/Amidakuji.test.ts
+++ packages/solidity-ts/tests/hardhat-tests/Amidakuji.test.ts
@@ -1,12 +1,16 @@
 import '~helpers/hardhat-imports';
 import '~tests/utils/chai-imports';
 
-import { expect } from 'chai';
-import { BigNumber } from 'ethers';
+// @ts-expect-error
+import { time } from '@openzeppelin/test-helpers';
+import chai, { expect } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
 import { Amidakuji__factory, Amidakuji } from 'generated/contract-types';
 import hre from 'hardhat';
 
 import { getHardhatSigners } from '~helpers/functions/accounts';
+
+chai.use(chaiAsPromised);
 
 describe('🚩 Challenge 0: Amidakuji', function () {
   this.timeout(180000);
@@ -15,7 +19,14 @@ describe('🚩 Challenge 0: Amidakuji', function () {
 
   describe('Amidakuji', function () {
     let amidaContract: Amidakuji;
-    let gameId: BigNumber | undefined;
+
+    async function setupGame(): Promise<void> {
+      const { user1, user2 } = await getHardhatSigners(hre);
+      await amidaContract.setup(1);
+      const gameId = await amidaContract.currentGameId();
+      await amidaContract.connect(user1).entry(gameId, 1);
+      await amidaContract.connect(user2).entry(gameId, 2);
+    }
 
     before(async () => {
       const { deployer } = await getHardhatSigners(hre);
@@ -29,51 +40,126 @@ describe('🚩 Challenge 0: Amidakuji', function () {
 
     describe('setup()', function () {
       it('Should be able to set a new game', async function () {
-        gameId = (await amidaContract.setup()).value;
-        expect(gameId).to.be.exist; // TODO make exact matcher
+        await amidaContract.setup(1);
+        const gameId = await amidaContract.currentGameId();
+        expect(gameId.toNumber()).to.be.equal(1);
+
+        await amidaContract.setup(2);
+        expect(await amidaContract.currentGameId()).to.be.equal(2);
       });
 
       it('Should be able to can call by only owner', async function () {
-        const { deployer } = await getHardhatSigners(hre);
-        gameId = (await amidaContract.setup()).value;
+        const { deployer, user1 } = await getHardhatSigners(hre);
+        await amidaContract.connect(deployer).setup(1);
+        await expect(amidaContract.connect(user1).setup(2)).to.be.rejected;
         expect(amidaContract.deployTransaction.from).to.be.equal(deployer.address);
-      });
-    });
-
-    describe('game()', function () {
-      it('Should be able to return the game info by id', async function () {
-        // if 'gameId' can be retrieved
-        const gameId = 1; // TODO temporary
-        expect(await amidaContract.game(gameId)).to.exist; // TODO make exact matcher
       });
     });
 
     describe('entry()', function () {
       it('Should be able to entry a user to the game', async function () {
-        const result = await amidaContract.entry();
-        expect(result).to.exist; // TODO make exact matcher
+        const { user1, user2 } = await getHardhatSigners(hre);
+        const gameId = await amidaContract.currentGameId();
+        await expect(amidaContract.connect(user1).entry(gameId, 1)).to.be.not.rejected;
+        await expect(amidaContract.connect(user2).entry(gameId, 2)).to.be.not.rejected;
+        const game = await amidaContract.game(gameId);
+        expect(game.players.length).to.equal(2);
+      });
+
+      it('Should be not able to entry a user to the game if pos is already exist', async function () {
+        const { user3 } = await getHardhatSigners(hre);
+        const gameId = await amidaContract.currentGameId();
+        await expect(amidaContract.connect(user3).entry(gameId, 1)).to.be.rejected;
+      });
+
+      it('Should be not able to entry a user to the game from duplicate address', async function () {
+        const { user1 } = await getHardhatSigners(hre);
+        const gameId = await amidaContract.currentGameId();
+        await expect(amidaContract.connect(user1).entry(gameId, 3)).to.be.rejected;
+      });
+
+      it('Should be not able to entry a user to the game if entry time ended', async function () {
+        const { user4 } = await getHardhatSigners(hre);
+        const gameId = await amidaContract.currentGameId();
+        await time.increase(time.duration.hours(24));
+        await expect(amidaContract.connect(user4).entry(gameId, 4)).to.be.rejected;
       });
     });
 
     describe('draw()', function () {
-      it('Should be able to draw a line between kuji', async function () {
-        const result = await amidaContract.draw(1, 3); // between 1 and 2 kuji and 3rd pos from top
-        expect(result); // TODO make exact matcher
+      it('Should be not able to draw a line if not drawing time started', async function () {
+        const { user1 } = await getHardhatSigners(hre);
+        await setupGame();
+        const gameId = await amidaContract.currentGameId();
+        await expect(amidaContract.connect(user1).draw(gameId, 1)).to.be.rejected;
+      });
+
+      it('Should be able to draw a line', async function () {
+        const { user1, user2 } = await getHardhatSigners(hre);
+        await time.increase(time.duration.hours(24));
+        const gameId = await amidaContract.currentGameId();
+        await expect(amidaContract.connect(user1).draw(gameId, 3)).to.be.not.rejected; // draw line to 3 to 4
+        await expect(amidaContract.connect(user2).draw(gameId, 1)).to.be.not.rejected; // draw line to 1 to 2
+        await expect(amidaContract.connect(user2).draw(gameId, 1)).to.be.not.rejected; // draw line to 1 to 2
+      });
+
+      it('Should be not able to draw a line if not entry', async function () {
+        const { user5 } = await getHardhatSigners(hre);
+        const gameId = await amidaContract.currentGameId();
+        await expect(amidaContract.connect(user5).draw(gameId, 1)).to.be.rejected;
+      });
+
+      it('Should be not able to draw a line if drawing time is ended', async function () {
+        const { user5 } = await getHardhatSigners(hre);
+        const gameId = await amidaContract.currentGameId();
+        await time.increase(time.duration.minutes(11));
+        await expect(amidaContract.connect(user5).draw(gameId, 1)).to.be.rejected;
       });
     });
 
     describe('reveal()', function () {
-      it('Should be able to move game data to public', async function () {
-        const result = await amidaContract.reveal();
-        expect(result); // TODO make exact matcher
+      it('Should be not able to reveal if not ended reveal time', async function () {
+        await setupGame();
+        const gameId = await amidaContract.currentGameId();
+        await expect(amidaContract.reveal(gameId)).to.be.rejected;
       });
-      // TODO check only owner can call
+
+      it('Should be not able to reveal if not owner', async function () {
+        const { user1 } = await getHardhatSigners(hre);
+        const gameId = await amidaContract.currentGameId();
+        await time.increase(time.duration.hours(25));
+        await expect(amidaContract.connect(user1).reveal(gameId)).to.be.rejected;
+      });
+
+      it('Should be not able to reveal by owner', async function () {
+        const gameId = await amidaContract.currentGameId();
+        await expect(amidaContract.reveal(gameId)).to.be.not.rejected;
+      });
     });
 
     describe('result()', function () {
-      it('Should be able to get game result (winner)', async function () {
-        const result = await amidaContract.result();
-        expect(result).to.exist; // TODO make exact matcher
+      it('Should be not able to get game result if not revealed', async function () {
+        await setupGame();
+        const gameId = await amidaContract.currentGameId();
+        await expect(amidaContract.result(gameId)).to.be.rejected;
+      });
+
+      it('Should be able to get game result', async function () {
+        await setupGame();
+        const { user1 } = await getHardhatSigners(hre);
+        const gameId = await amidaContract.currentGameId();
+        await time.increase(time.duration.hours(25));
+        await amidaContract.reveal(gameId);
+        const result = await amidaContract.connect(user1).result(gameId);
+        expect(result).to.exist;
+      });
+    });
+
+    describe('game()', function () {
+      it('Should be able to return the game info by id', async function () {
+        await setupGame();
+        const gameId = await amidaContract.currentGameId();
+        expect(await amidaContract.game(gameId)).to.exist;
       });
     });
   });
